b335949e36a122e009e3ed51839be449bf2cf693
synacktra.work@gmail.com <synacktra.work@gmail.com>
2025-09-03 00:57:51 +0530

    feat: add custom exceptions sub module

---
+++ strot/exceptions.py
@@ -0,0 +1,5 @@
+class RequestException(Exception):
+    def __init__(self, status_code: int, message: str) -> None:
+        self.status_code = status_code
+        self.message = message
+        super().__init__(message)
