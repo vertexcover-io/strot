27ef0ec28290394ab2c086402221612afa786ee4
synacktra.work@gmail.com <synacktra.work@gmail.com>
2025-09-04 18:08:11 +0530

    fix: reset other pagination values when detecting start page and cursor

--- strot/pagination_translators/base.py
+++ strot/pagination_translators/base.py
@@ -20,7 +20,13 @@ class BasePaginationTranslator:
         if pg_info.page is None:
             raise ValueError("Pagination info must have a page parameter")
 
-        data = await self._fetch_data(request_detail, response_detail, parameters={pg_info.page.key: "0"})
+        state = {}
+        if pg_info.cursor:
+            state[pg_info.cursor.key] = None
+        if pg_info.offset:
+            state[pg_info.offset.key] = "0"
+
+        data = await self._fetch_data(request_detail, response_detail, parameters=state | {pg_info.page.key: "0"})
         return 0 if data else 1
 
     async def detect_start_cursor(self, request_detail: RequestDetail, response_detail: ResponseDetail) -> str | None:
@@ -30,9 +36,13 @@ class BasePaginationTranslator:
         if pg_info.cursor is None:
             raise ValueError("Pagination info must have a cursor parameter")
 
+        state = {}
+        if pg_info.page:
+            state[pg_info.page.key] = None
+
         # Try 1: No cursor (some APIs support starting without cursor)
         try:
-            if await self._fetch_data(request_detail, response_detail, parameters={pg_info.cursor.key: None}):
+            if await self._fetch_data(request_detail, response_detail, parameters=state | {pg_info.cursor.key: None}):
                 return None
         except Exception:  # noqa: S110
             pass
@@ -40,8 +50,8 @@ class BasePaginationTranslator:
         # Try 2: Replace sub cursors with null to create a "first page" cursor
         try:
             nullable_cursor = pg_info.cursor.get_nullable_cursor()
-            if await self._fetch_data(
-                request_detail, response_detail, parameters={pg_info.cursor.key: nullable_cursor}
+            if nullable_cursor is not None and await self._fetch_data(
+                request_detail, response_detail, parameters=state | {pg_info.cursor.key: nullable_cursor}
             ):
                 return nullable_cursor
         except Exception:  # noqa: S110
