b42a6a54e3006426ea68cfb7b833b0054e81b600
synacktra.work@gmail.com <synacktra.work@gmail.com>
2025-09-04 18:01:58 +0530

    fix: make fields optional

--- strot/schema/response/response.py
+++ strot/schema/response/response.py
@@ -3,10 +3,10 @@ from pydantic import BaseModel
 from strot.schema.request import Request
 from strot.schema.response.preprocessor import ResponsePreprocessorUnion
 
-__all__ = "Response"
+__all__ = ("Response",)
 
 
 class Response(BaseModel):
     request: Request
-    value: str
+    value: str = ""
     preprocessor: ResponsePreprocessorUnion | None = None
