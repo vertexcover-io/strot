6348032401d2baeebacf2458d9c2a103a5c0fb27
synacktra.work@gmail.com <synacktra.work@gmail.com>
2025-09-04 18:01:58 +0530

    fix: make fields optional

--- strot/schema/request/request.py
+++ strot/schema/request/request.py
@@ -1,5 +1,7 @@
 from typing import Any, Literal
 
+from pydantic import Field
+
 from strot.schema.base import BaseSchema
 
 __all__ = ("Request",)
@@ -11,6 +13,6 @@ class Request(BaseSchema):
     method: str
     url: str
     type: RequestType = "ajax"
-    queries: dict[str, str]
-    headers: dict[str, str]
+    queries: dict[str, str] = Field(default_factory=dict)
+    headers: dict[str, str] = Field(default_factory=dict)
     post_data: dict[str, Any] | str | None = None
